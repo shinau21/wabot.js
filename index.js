8f8d022cbd91f66abfee4fc28ef3d08657742558
shinau21 <bandingandesa@gmail.com>
2022-08-30 11:34:40 +0700

    Menambahkan fitur creategroup dengan file sebagai peserta

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
+const fs =  require("fs")
  
 const client = new Client({
   authStrategy: new LocalAuth(),
@@ -13,20 +14,55 @@ client.on("ready", () => {
     console.log("Client Ready")
 })
 
+client.on("message", async(message) => {
+})
 client.on('message_create', async(message) => {
-    let chat = await message.getChat()
-    console.log(chat)
-    if(message.body == '!kickall') {
-        let chat = await message.getChat()
-        const members = chat.participants
-        for (let member of members) {
-            try{
-                if(member.id._serialized != member.owner._serialized){
-                    chat.removeParticipants([member.id._serialized])
+    if(message.fromMe){
+        if(message.hasMedia && message.type == 'document'){
+            const attachmentData = await message.downloadMedia();
+            fs.writeFile("./upload/document/" + attachmentData.filename,attachmentData.data,"base64",function (err) {
+                if (err) {
+                    console.log(err);
                 }
-            } catch(e) {
-                console.log(e)
             }
+            );
+            var fsReadStream = fs.createReadStream("./upload/document/" + attachmentData.filename, 'base64');
+        }
+        if(message.body.startsWith('!kickall')) {
+            let chat = await message.getChat()
+            if(chat.isGroup){
+                try{
+                    const members = chat.participants
+                    for (let member of members) {
+                        try{
+                            if(member.id._serialized != member.owner._serialized){
+                                chat.removeParticipants([member.id._serialized])
+                            }
+                        } catch(e) {
+                            console.log(e)
+                        }
+                    }    
+                } catch(e){
+                    client.sendMessage(message.from,"Kamu bukan Admin")
+                }
+            } else {
+                client.sendMessage(message.from,"Gunakan di grup")
+            }
+        }
+        else if(message.body.startsWith('!creategroup')){
+            let file = message.body.split(' ')[1];
+            let group = message.body.split(' ')[2];
+            fs.readFile("./upload/document/" + file, function(err,data) {
+                let content = data.toString();
+                let numbers = content.split(',');
+                console.log(content);
+                let members = []
+                for (let x in numbers){
+                    members.push(numbers[x].includes('@c.us') ? numbers[x] : `${numbers[x]}@c.us`)
+                }
+                console.log(members)
+                client.createGroup(group,members)
+            });
         }
     }
 })
